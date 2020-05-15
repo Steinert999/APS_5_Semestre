b9a47b6be10346139502776a70e1eb054f4fda70
Lavarda <vitorlavarda.souza@gmail.com>
2020-05-15 20:59:21 -0300

    Starting structure for connections

--- app.js
+++ app.js
@@ -1,5 +1,46 @@
+// imports
+const mongoose = require('mongoose');
 const express = require("express");
 const port = 8080;
 const app = express();
+const bodyParser = require('body-parser')
+const cors = require('cors');
+
+/**
+ * Connect to db with mongoose
+ *  mongoose.connect('', {
+        useNewUrlParser: true,
+    });
+*/
+
+// Modelo requisições 
+
+app.use(bodyParser.json())
+app.use(cors());
+
+app.get('/', (req,res) => {
+     res.json('Entrou')
+})
+
+let cities = ''
+
+app.get(`${cities}`, (req,res) => {
+    res.json(cities)
+})
+
+let state = ''
+app.get(`${state}`, (req,res) => {
+    res.json(state)
+})
+
+let rota = ''
+app.post(`${rota}`, (req, res) => {
+    list.push(req.body)
+    res.json({ status: '' })
+})
+
+app.get('https://api.spotify.com/v1/albums', (req,res) => {
+    console.log("res", JSON.stringify(res))
+})
 
 app.listen(port, () => console.log(`App started in port ${port}`));
---
+++ src/routes.js
@@ -0,0 +1,9 @@
+const expres = require('express');
+
+const routes = new expres.Router();
+
+// MIDDLEWARE, interceptador de chamadas do servidor. ( as rotas )
+// routes.post('/posts',);
+// routes.get('https://api.spotify.com/v1/albums');
+
+module.exports = routes;
