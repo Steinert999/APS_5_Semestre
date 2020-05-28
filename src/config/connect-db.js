f552a525e7a76fd2d5c776fe0e07ad3861823a5f
Guilherme Steinert de Azevedo <steinert999@outlook.com>
2020-05-27 21:25:21 -0300

    new db connection

--- src/config/connect-db.js
+++ src/config/connect-db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 exports.connect = async () => {
     console.log("STARTING CONNECTION")
     try {
-        await mongoose.connect("mongodb+srv://Lavarda:VI2107vivi@@aps-5-semestre-g6df3.mongodb.net/test?retryWrites=true&w=majority", {
+        await mongoose.connect("mongodb+srv://steinert:aps5semestre@coronavirus-cluster-mhhob.mongodb.net/test?retryWrites=true&w=majority", {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
