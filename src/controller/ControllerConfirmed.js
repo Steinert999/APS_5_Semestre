e3a1e312c37cf2842c805f83683602b8f9f9960b
Guilherme Steinert de Azevedo <steinert999@outlook.com>
2020-05-27 21:27:36 -0300

    fixing func params

--- src/controller/ControllerConfirmed.js
+++ src/controller/ControllerConfirmed.js
@@ -1,6 +1,6 @@
 const service = require('../services/DataService');
 
-const getData = async (req,res,next) => service.getData();
+const getData = async (req,res,next) => service.getData(req, res, next);
 
 module.exports = {
     getData,
--- src/controller/ControllerSul.js
+++ src/controller/ControllerSul.js
@@ -1,6 +1,6 @@
 const service = require('../services/DataService');
 
-const getSouthData = async (req,res,next) => service.getSouthData();
+const getSouthData = async (req,res,next) => service.getSouthData(req, res, next);
 
 module.exports = {
     getSouthData
